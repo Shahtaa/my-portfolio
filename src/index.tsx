e479405bcbb5784894687899e9baede07c7f4a08
Alexei <>
2024-09-28 21:03:49 +0300

    mui setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
+// src/index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './App'; // Use default import
+import './index.css'; // Make sure to import your CSS for global styles
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, you can add
-// performance measurement logic here, but for now, we are removing
-// reportWebVitals since we are not using it anymore.
