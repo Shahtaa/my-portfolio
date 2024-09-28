8e86b520f37e9118f7a40b31d77caeb2f4edf09d
Alexei <>
2024-09-28 20:41:34 +0300

    Initial Setup: Create React App with TypeScript

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, you can add
+// performance measurement logic here, but for now, we are removing
+// reportWebVitals since we are not using it anymore.
