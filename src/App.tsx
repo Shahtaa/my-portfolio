e84a3b829bea35abe01f9c8c824cbd212f0cd666
Alexei <>
2024-09-29 09:01:41 +0300

    404

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 // src/App.tsx
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Container, Box, Typography } from '@mui/material';
+import { Container, Box } from '@mui/material';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import NotFound from './components/NotFound'; // Import the NotFound component
 
 const App: React.FC = () => {
   return (
-    <Container maxWidth="lg"> {/* Set a maxWidth for the container */}
+    <Container maxWidth="lg">
       <Header />
       <Box mt={4}>
         <Routes>
@@ -17,7 +18,7 @@ const App: React.FC = () => {
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Typography variant="h5" align="center">404 Not Found</Typography>} />
+          <Route path="*" element={<NotFound />} /> {/* Use NotFound component */}
         </Routes>
       </Box>
     </Container>
